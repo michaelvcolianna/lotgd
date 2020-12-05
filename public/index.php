0de49f85d747e919b63363fd42a443277758527d
MVC <michael.colianna@me.com>
2020-12-04 22:18:21 -0500

    Undoes some of the messy start - Adds the bootstrap include back to all files - Creates a front controller specifically - Points htaccess to the front controller as needed - Returns original index - Returns installer stage 6 to the .env variables - Removes test views - Creates includes for the current templates

--- public/index.php
+++ public/index.php
@@ -1,44 +1,6 @@
 <?php
-
-$logged_in = session('authenticated') == $_ENV['APP_AUTH'] ?? 'lotgd';
-
-// home doesn't exist, just a test for this fake login process
-$template = $logged_in
-    ? 'home'
-    : 'login'
-;
-
-$errors = [];
-
-if(!$logged_in && !empty($_POST))
-{
-    $db = DB::getInstance();
-    $db->doQuery("select `acctid`, `password` from `accounts` where `login` = ?", [
-        $_POST['username'],
-    ]);
-    $user = $db->getNext();
-    $db = null;
-
-    if($user && password_verify($_POST['password'], $user->password))
-    {
-        session('authenticated', $_ENV['APP_AUTH'] ?? 'lotgd');
-        session('user', $user->acctid);
-
-        header('Location:/');
-        exit();
-    }
-    else
-    {
-        $errors[] = 'The supplied credentials don’t match. Please verify the spelling.';
-    }
-}
-
-echo $twig->render('pages/' . $template . '.html.twig', [
-    'errors' => $errors,
-    'navigation' => [
-        [
-            'href' => '#',
-            'text' => 'Create an account',
-        ],
-    ],
-]);
+// translator ready
+// addnews ready
+// mail ready
+header("Location: home.php?".$_SERVER['QUERY_STRING']);
+?>
